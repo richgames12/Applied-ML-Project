5fb0204a6f3790cfadf173e703b300c21ac47f2d
Richard Douma <r.j.douma.6@student.rug.nl>
2025-04-30 16:38:19 +0200

    Make two plots

--- create_plots.py
+++ create_plots.py
@@ -6,14 +6,22 @@ import librosa.display
 
 #plot the first audio file in the dataset
 dataset_path = 'data/uwrfkaggler/ravdess-emotional-speech-audio/versions/1'
-first_audio_file = os.path.join(dataset_path, 'Actor_01', '03-01-01-01-01-01-01.wav')
-def plot_audio_file(file_path, sr=22050):
+first_audio_file = os.path.join(dataset_path, 'Actor_01', '03-01-02-02-01-01-01.wav')
+def plot_audio_file(file_path, sr=44100):
     # Load the audio file
     y, sr = librosa.load(file_path, sr=sr)
 
     # Create a time variable for plotting
     time = np.linspace(0, len(y) / sr, len(y))
     spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+
+    # plot the waveform
+    plt.figure(figsize=(10, 4))
+    plt.plot(time, y)
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.title('Waveform')
+    plt.show()
 
     # Plot the spectrogram
     plt.figure(figsize=(10, 4))
