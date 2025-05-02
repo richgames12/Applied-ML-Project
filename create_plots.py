0fa8358bdc4f163b792c7fd28bf23c11d1c7dedc
Richard Douma <r.j.douma.6@student.rug.nl>
2025-05-02 15:24:45 +0200

    Add requirements file and Plot t-sne of spectrogram.

--- create_plots.py
+++ create_plots.py
@@ -1,23 +1,30 @@
 import kagglehub
+from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import os
 import numpy as np
 import librosa.display
 
 #plot the first audio file in the dataset
-dataset_path = 'data/uwrfkaggler/ravdess-emotional-speech-audio/versions/1'
-first_audio_file = os.path.join(dataset_path, 'Actor_01', '03-01-02-02-01-01-01.wav')
-def plot_audio_file(file_path, sr=44100):
+dataset_path = 'project_name/data/uwrfkaggler/ravdess-emotional-speech-audio/versions/1'
+first_audio_file = os.path.join(dataset_path, 'Actor_01', '03-01-02-01-01-01-01.wav')
+
+def plot_audio_file(file_path: str, sampling_rate: float=44100):
     # Load the audio file
-    y, sr = librosa.load(file_path, sr=sr)
+    time_series, sampling_rate = librosa.load(file_path, sr=sampling_rate)
 
     # Create a time variable for plotting
-    time = np.linspace(0, len(y) / sr, len(y))
-    spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+    samples = np.linspace(0, len(time_series) / sampling_rate, len(time_series))
+    spectrogram = librosa.feature.melspectrogram(y=time_series, sr=sampling_rate, n_mels=128, fmax=8000)
+
+    # Reduce dimensionality with T-SNE
+    tsne = TSNE(n_components=2, random_state=42)
+    transformed_spectrogram = spectrogram.T
+    reduced_spectrogram = tsne.fit_transform(transformed_spectrogram)
 
     # plot the waveform
     plt.figure(figsize=(10, 4))
-    plt.plot(time, y)
+    plt.plot(samples, time_series)
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
     plt.title('Waveform')
@@ -25,11 +32,22 @@ def plot_audio_file(file_path, sr=44100):
 
     # Plot the spectrogram
     plt.figure(figsize=(10, 4))
-    librosa.display.specshow(librosa.power_to_db(spectrogram, ref=np.max), y_axis='mel', x_axis='time', sr=sr, fmax=8000)
+    librosa.display.specshow(
+        librosa.power_to_db(spectrogram, ref=np.max), y_axis='mel', x_axis='time', sr=sampling_rate, fmax=8000
+    )
     plt.colorbar(format='%+2.0f dB')
     plt.xlabel('Time')
     plt.ylabel('Mel frequency')
     plt.title('Spectrogram')
+    plt.show()
+    
+    #plot the reduced spectrogram
+    plt.figure(figsize=(10, 4))
+    plt.scatter(reduced_spectrogram[:, 0], reduced_spectrogram[:, 1], c='blue', alpha=0.5)
+    plt.title('T-SNE of Mel Spectrogram')
+    plt.xlabel('Component 1')
+    plt.ylabel('Component 2')
+    plt.grid()
     plt.show()
 
 if __name__ == "__main__":
