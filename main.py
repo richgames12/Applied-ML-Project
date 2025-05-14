239c5e787a78503c5326bd3f17f262971158e7cc
Richard Douma <r.j.douma.6@student.rug.nl>
2025-05-14 10:55:13 +0200

    Add download dataset file and update gitignore correctly.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# This is a sample Python script.
-
 def hello_world():
     print("Hello, World!")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     hello_world()
+    # This is a test to check if the main.py file is working correctly.
