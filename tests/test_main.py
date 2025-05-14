b38b6230d3189b3f3cf3ad071cf1b69ec6259070
jessekerkhof <j.j.kerkhof@student.rug.nl>
2025-05-14 10:42:22 +0200

    Testing something
    initializing github

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from main import hello_world
 
 class MainTest(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello_world(), "Hello, World!")
+        self.assertEqual(hello_world(), "Hello, World!, Made a test")
 
 
 if __name__ == '__main__':
