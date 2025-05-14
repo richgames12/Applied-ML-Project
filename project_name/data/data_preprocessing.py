c2ba13f73d5a8d02d532a6414988824a4ee8c243
Remco <r.broekhuizen@student.rug.nl>
2025-05-14 20:06:25 +0200

    Add a standardized length method

--- project_name/data/data_preprocessing.py
+++ project_name/data/data_preprocessing.py
@@ -8,8 +8,8 @@ class AudioPreprocessor:
             self,
             sampling_rate: int = 22050,
             target_length: int = 66150,
-            # When using a CNN we should also include the option to select a
-            # different data augmenter for spectrograms
+            # TODO: When using a CNN we should also include the option to
+            # select a different data augmenter for spectrograms
             data_augmenter: RawAudioAugmenter | None = None,
             use_spectrograms: bool = False
     ) -> None:
@@ -32,7 +32,7 @@ class AudioPreprocessor:
 
         # Process the raw data
         augmented_raw_audio = self._augment_raw_audio(raw_audio)
-        return self.standardize_length(augmented_raw_audio)
+        return self._standardize_length(augmented_raw_audio)
 
     def process_all_files(self, file_paths):
         pass
@@ -52,12 +52,33 @@ class AudioPreprocessor:
     def _augment_raw_audio(self, audio):
         pass
 
-    def _get_spectogram(self, audio):
+    def _get_spectogram(self, audio: np.ndarray) -> np.ndarray:
+        # TODO: Will have to be implemented later for CNN
         raise NotImplementedError("Get spectrograms is still in progress.")
 
-    def _augment_spectogram(self, spectogram):
-        # Will have to be implemented later
+    def _augment_spectogram(self, spectrogram: np.ndarray) -> np.ndarray:
+        # TODO: Will have to be implemented later for CNN
         raise NotImplementedError("Augment spectrogram is still in progress.")
 
-    def _standardize_length(self, audio):
-        pass
+    def _standardize_length(self, audio: np.ndarray) -> np.ndarray:
+        """Pad or trim the raw audio data to make it the right lenght.
+
+        Args:
+            audio (np.ndarray): The raw audio for which the length needs to be
+                standardized.
+
+        Returns:
+            np.ndarray: The raw audio with the standardized length.
+        """
+        current_length = len(audio)
+        if current_length < self.target_length:
+            # Audio is too short so pad where necessary
+            padding = np.zeros(self.target_length - current_length)
+            return np.concatenate((audio, padding))
+
+        elif current_length > self.target_length:
+            # Audio is too long so cut the end off.
+            return audio[:self.target_length]
+
+        # The audio is exactly the right length
+        return audio
