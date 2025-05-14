38231cb35ade6d3dff14c50d168470e90b95d777
Remco <r.broekhuizen@student.rug.nl>
2025-05-14 20:20:27 +0200

    Add  an augment data method in the preprocessing class

--- project_name/data/data_preprocessing.py
+++ project_name/data/data_preprocessing.py
@@ -25,14 +25,15 @@ class AudioPreprocessor:
             # Process the spectograms
             spectrogram = self._get_spectogram(raw_audio)
             if spectrogram is not None:
-                augmented_spectogram = self._augment_spectogram(spectrogram)
-                # TODO: Maybe standardize the length of the spectrogram
-                return augmented_spectogram
+                augmented_spectogram = self._augment_data(spectrogram)
+                return self._standardize_spectrogram_length(
+                    augmented_spectogram
+                )
             return None
 
         # Process the raw data
-        augmented_raw_audio = self._augment_raw_audio(raw_audio)
-        return self._standardize_length(augmented_raw_audio)
+        augmented_raw_audio = self._augment_data(raw_audio)
+        return self._standardize_raw_length(augmented_raw_audio)
 
     def process_all_files(self, file_paths):
         pass
@@ -49,19 +50,24 @@ class AudioPreprocessor:
         """
         return librosa.load(file_path, sr=self.sampling_rate)[0]
 
-    def _augment_raw_audio(self, audio):
-        pass
+    def _augment_data(self, data: np.ndarray) -> np.ndarray:
+        """Augment either the raw audio or spectogram.
 
-    def _get_spectogram(self, audio: np.ndarray) -> np.ndarray:
-        # TODO: Will have to be implemented later for CNN
-        raise NotImplementedError("Get spectrograms is still in progress.")
+        Args:
+            data (np.ndarray): Either the raw audio when a raw audio augmenter
+                is given or a spectrogram when a spectrogram augmenter is
+                given. The right augmenter should be provided.
 
-    def _augment_spectogram(self, spectrogram: np.ndarray) -> np.ndarray:
-        # TODO: Will have to be implemented later for CNN
-        raise NotImplementedError("Augment spectrogram is still in progress.")
+        Returns:
+            np.ndarray: The augmented raw audio/ spectrogram.
+        """
+        if self.data_augmenter:
+            return self.data_augmenter(data)
+        return data
 
-    def _standardize_length(self, audio: np.ndarray) -> np.ndarray:
-        """Pad or trim the raw audio data to make it the right lenght.
+    def _standardize_raw_length(self, audio: np.ndarray) -> np.ndarray:
+        """
+        Pad or trim the raw audio data to make it the right lenght.
 
         Args:
             audio (np.ndarray): The raw audio for which the length needs to be
@@ -75,10 +81,19 @@ class AudioPreprocessor:
             # Audio is too short so pad where necessary
             padding = np.zeros(self.target_length - current_length)
             return np.concatenate((audio, padding))
-
         elif current_length > self.target_length:
             # Audio is too long so cut the end off.
             return audio[:self.target_length]
-
         # The audio is exactly the right length
         return audio
+
+    def _get_spectogram(self, audio: np.ndarray) -> np.ndarray:
+        raise NotImplementedError("Get spectrograms is still in progress.")
+
+    def _augment_spectogram(self, spectrogram: np.ndarray) -> np.ndarray:
+        raise NotImplementedError("Augment spectrogram is still in progress.")
+
+    def _standardize_spectrogram_length(
+            self, spectrogram: np.ndarray
+    ) -> np.ndarray:
+        raise NotImplementedError("Standardize is still in progress.")
