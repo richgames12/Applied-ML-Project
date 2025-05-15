3dbf530173989ca14a17b9c28477deeb8ec81462
Richard Douma <r.j.douma.6@student.rug.nl>
2025-05-15 12:43:00 +0200

    Add global constant for the file path of the dataset.

--- download_dataset.py
+++ download_dataset.py
@@ -2,13 +2,15 @@ import kagglehub
 import os
 import shutil
 
-path = kagglehub.dataset_download("uwrfkaggler/ravdess-emotional-speech-audio")
+if __name__ == "__main__":
+    path = kagglehub.dataset_download("uwrfkaggler/ravdess-emotional-speech-audio")
 
-# Move the dataset to a specific directory
-destination_dir = os.path.join(os.getcwd(), "project_name", "data", "uwrfkaggler", "ravdess-emotional-speech-audio", "versions")
-if not os.path.exists(destination_dir):
-    os.makedirs(destination_dir)
-source_dir = path
+    # Move the dataset to a specific directory
+    destination_dir = os.path.join(os.getcwd(), "project_name", "data", "uwrfkaggler", "ravdess-emotional-speech-audio", "versions")
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+    source_dir = path
 
-shutil.move(source_dir, destination_dir)
-print("Path to dataset files:", path)
+    DATASET_DIR = os.path.join(destination_dir, "1")
+    shutil.move(source_dir, destination_dir)
+    print("Path to dataset files:", DATASET_DIR)
