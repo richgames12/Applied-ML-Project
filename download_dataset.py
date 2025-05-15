e0c47dfc3328afdbccf055765cd707af9d66a55d
Richard Douma <r.j.douma.6@student.rug.nl>
2025-05-15 12:36:20 +0200

    Move dataset to correct location after downloading.

--- download_dataset.py
+++ download_dataset.py
@@ -1,5 +1,14 @@
 import kagglehub
+import os
+import shutil
 
 path = kagglehub.dataset_download("uwrfkaggler/ravdess-emotional-speech-audio")
 
+# Move the dataset to a specific directory
+destination_dir = os.path.join(os.getcwd(), "project_name", "data", "uwrfkaggler", "ravdess-emotional-speech-audio", "versions")
+if not os.path.exists(destination_dir):
+    os.makedirs(destination_dir)
+source_dir = path
+
+shutil.move(source_dir, destination_dir)
 print("Path to dataset files:", path)
