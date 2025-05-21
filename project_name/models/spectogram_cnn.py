bf4d040938682d2ede6c5f821b445e8373a82750
Remco <r.broekhuizen@student.rug.nl>
2025-05-21 21:18:34 +0200

    Add multiclass functionality

--- project_name/models/spectogram_cnn.py
+++ project_name/models/spectogram_cnn.py
@@ -2,20 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class EmotionCNN(nn.Module):
-    def __init__(self, num_classes=8, in_channels=1):
-        """Initialize the CNN blocks.
+class MultiheadEmotionCNN(nn.Module):
+    def __init__(self, num_emotions=8, num_intensity=2, in_channels=1):
+        """Initialize the CNN blocks."""
+        super(MultiheadEmotionCNN, self).__init__()
 
-        Args:
-            num_classes (int, optional): The number of emotions in the dataset.
-                Defaults to 8.
-            in_channels (int, optional): The number of layers of the
-                spectogram. 1 if 2D, 3 if RGB. Defaults to 1.
-        """
-        super(EmotionCNN, self).__init__()
-
+        # Shared feature extraction blocks
         self.conv_block1 = nn.Sequential(
-            nn.Conv2d(in_channels, 16, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels, 16, kernel_size=3, padding=1),  # Expects 2D
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2)
@@ -35,18 +29,27 @@ class EmotionCNN(nn.Module):
             nn.MaxPool2d(2)
         )
 
+        # Shared fully connected layer
         self.dropout = nn.Dropout(0.3)
-        self.fc1 = nn.Linear(64 * 16 * 16, 256)
-        self.fc2 = nn.Linear(256, num_classes)
+        self.fc_shared = nn.Linear(64 * 16 * 16, 256)
+
+        # Emotion classification head
+        self.fc_emotion = nn.Linear(256, 8)
+
+        # Intensity classification head
+        self.fc_intensity = nn.Linear(256, 2)
 
     def forward(self, x):
-        x = self.conv_block1(x)  # Conv + BN + ReLU + Pool
+        # Shared Conv + BN + ReLU + Pool
+        x = self.conv_block1(x)
         x = self.conv_block2(x)
         x = self.conv_block3(x)
 
+        # Shared fully connected
         x = x.view(x.size(0), -1)  # Flatten the convolutional output
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+        x = self.dropout(F.relu(self.fc_shared(x)))
+
+        # Two separate fully connected heads
+        emotion_logits = self.fc_emotion(x)
+        intensity_logits = self.fc_intensity(x)
+        return emotion_logits, intensity_logits
