de080a0345aa79ea73db85c5a24cacd7be58c436
Remco <r.broekhuizen@student.rug.nl>
2025-05-28 21:29:18 +0200

    Add intensity SVM to main.py and reordered main.py to make it much more orderly.

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from project_name.data.data_preprocessing import AudioPreprocessor
 from project_name.data.data_augmentation import RawAudioAugmenter
 from project_name.features.audio_feature_extractor import AudioFeatureExtractor
 from project_name.models.audio_feature_svm import AudioFeatureSVM
-from project_name.models.spectrogram_cnn import MultiheadEmotionCNN
-from project_name.models.spectrogram_cnn import fit_model
 from sklearn.multiclass import OneVsRestClassifier
 
-import torch
 import numpy as np
 import random
-from torch.utils.data import DataLoader, TensorDataset
+
 
 if __name__ == "__main__":
     seed = None
@@ -19,9 +16,16 @@ if __name__ == "__main__":
     np.random.seed(seed)
     random.seed(seed)
 
+    # ____________________________________________
+    #                 Data Loading
+    # ____________________________________________
     # Initialize the data file splitter
     splitter = DataFileSplitter(dataset_path=DATASET_DIR, seed=seed)
     train_data, val_data, test_data = splitter.get_data_splits_copy()
+
+    # ____________________________________________
+    #              Data Preprocessing
+    # ____________________________________________
 
     # Initialize the raw audio augmenter
     augmenter = RawAudioAugmenter(pitch_probability=0)
@@ -30,12 +34,21 @@ if __name__ == "__main__":
     preprocessor = AudioPreprocessor(
         data_augmenter=augmenter, n_augmentations=2
     )
-    
 
     # Process the training data
     train_processed, train_emotion_labels, train_intensity_labels = \
         preprocessor.process_all(train_data)
     print("Training data processed.")
+
+    # Preprocess the test data
+    preprocessor.data_augmenter = None  # Test data should not be augmented
+    test_processed, test_emotion_labels, test_intensity_labels = \
+        preprocessor.process_all(test_data)
+    print("Test data processed.")
+
+    # ____________________________________________
+    #             Feature Extraction
+    # ____________________________________________
 
     # Initialize the audio feature extractor
     feature_extractor = AudioFeatureExtractor(use_deltas=True, n_mfcc=20)
@@ -44,16 +57,13 @@ if __name__ == "__main__":
     train_features = feature_extractor.extract_features_all(train_processed)
     print("Training features extracted.")
 
+    # Extract features from the processed test data
+    test_features = feature_extractor.extract_features_all(test_processed)
+    print("Test features extracted.")
 
-
-    #Create tensor dataset to be used with dataloader 
-    dataset = TensorDataset(train_features, train_emotion_labels, train_intensity_labels)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-    #Model train test
-    CNN = MultiheadEmotionCNN()
-    fit_model(CNN, dataloader, 10)
-
+    # ____________________________________________
+    #               Model Training
+    # ____________________________________________
 
     # Shuffle the data before training the SVM
     indices = np.arange(len(train_features))
@@ -62,38 +72,46 @@ if __name__ == "__main__":
     train_emotion_labels = train_emotion_labels[indices]
     train_intensity_labels = train_intensity_labels[indices]
 
-
-
-
     # Initialize and train the SVM for emotion recognition
     # Use a OneVsRest version to increase the models accuracy
     base_emotion_svm = AudioFeatureSVM(
         probability=True, regularization_parameter=10, seed=seed
     )
+
+    base_intensity_svm = AudioFeatureSVM(
+        probability=False, regularization_parameter=10, seed=seed
+    )
+
+    print("Training Emotion SVM.")
     multiclass_emotion_svm = OneVsRestClassifier(base_emotion_svm)
     multiclass_emotion_svm.fit(train_features, train_emotion_labels)
     print("Emotion SVM trained.")
 
-    # Process the test data
-    preprocessor.data_augmenter = None  # Test data should not be augmented
-    test_processed, test_emotion_labels, test_intensity_labels = \
-        preprocessor.process_all(test_data)
-    print("Test data processed.")
+    print("Training Intensity SVM.")
+    # Only two classes so no OneVsRest
+    base_intensity_svm.fit(train_features, train_intensity_labels)
+    print("Intensity SVM trained.")
 
-    # Extract features from the processed test data
-    test_features = feature_extractor.extract_features_all(test_processed)
-    print("Test features extracted.")
+    # ____________________________________________
+    #              Model Evaluation
+    # ____________________________________________
 
     if test_features.shape[0] > 0:
+        # Evaluate emotion SVM
         emotion_accuracy = multiclass_emotion_svm.score(
             test_features, test_emotion_labels
+        )
+        # Evaluate intensity SVM
+        intensity_accuracy = base_intensity_svm.score(
+            test_features, test_intensity_labels
         )
         print(
             "Emotion recognition accuracy on the test set:",
             f"{emotion_accuracy:.4f}"
         )
+        print(
+            "Intensity recognition accuracy on the test set:",
+            f"{intensity_accuracy:.4f}"
+        )
     else:
         print("No test data available for evaluation.")
-
-
-    #C
